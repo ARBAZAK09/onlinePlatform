0d412baade72c8bb8222c55d318b1862252026fd
ARBAZAK09 <khanjawwad620@gmail.com>
2022-02-14 16:49:22 +0530

    committed

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -3,6 +3,7 @@ import "./main.css";
 const Contactus = () => {
   return (
     <div className="contactus">
+      <a href="https://www.facebook.com/">facebook</a>
       <img src="contactus.png"></img>
     </div>
   );
