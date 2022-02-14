7e647611b07f3052aae8d8524540ba2ae81558f7
ARBAZAK09 <khanjawwad620@gmail.com>
2022-02-14 16:26:28 +0530

    committed

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Aboutus = () => {
   return (
     <div>
+      <a href="https://www.facebook.com/">facebook</a>
       <img src="aboutus.png"></img>
     </div>
   );
